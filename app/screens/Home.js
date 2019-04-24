22a2782688d3ef859d39be888a75f2e182e4f2cd
Aditya Kapoor <adityakapoor.mait@gmail.com>
2019-04-24 16:05:00 +0530

    Add final touches.

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -8,8 +8,9 @@ import { ClearButton } from '../components/Buttons';
 import { LastConverted } from '../components/Text';
 import { Header } from '../components/Header';
 import PropTypes from 'prop-types';
+import { connectAlert } from '../components/Alert'
 
-import { swapCurrency, changeCurrencyAmount } from '../actions/currencies';
+import { swapCurrency, changeCurrencyAmount, getInitialConversion } from '../actions/currencies';
 
 class Home extends Component {
   static propTypes = {
@@ -22,6 +23,18 @@ class Home extends Component {
     isFetching: PropTypes.bool,
     lastConvertedDate: PropTypes.object,
     primaryColor: PropTypes.string,
+    alertWithType: PropTypes.func,
+    currencyError: PropTypes.string,
+  }
+
+  componentWillMount() {
+    this.props.dispatch(getInitialConversion())
+  }
+
+  componentsWillReceiveProps(nextProps) {
+    if(nextProps.currencyError && nextProps.currencyError !== this.props.currencyError) {
+      this.props.alertWithType('error', 'Error', nextProps.currencyError)
+    }
   }
 
   handlePressBaseCurrency = () => {
@@ -97,7 +110,8 @@ const mapStateToProps = (state) => {
     isFetching: conversionSelector.isFetching,
     lastConvertedDate: conversionSelector.date ? new Date(conversionSelector.date) : new Date(),
     primaryColor: state.theme.primaryColor,
+    currencyError: state.currencyError,
   };
 };
 
-export default connect(mapStateToProps)(Home);
+export default connect(mapStateToProps)(connectAlert(Home));
